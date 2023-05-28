b3fdbe2dc0af74a2268c527f7fd8bdc8ec25227b
JaviMedac <jtc0006@alu.medac.es>
2023-05-28 20:41:57 +0200

    fhgfh

---
+++ YOSHILIST/src/Vista/Main.java
@@ -0,0 +1,390 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package Vista;
+
+import Controlador.*;
+import Modelo.Articulo;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.util.Scanner;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Panel
+     */
+    private ConexionSQL conex;
+    FondoPanel1 fondo = new FondoPanel1();
+    //private String bbdd;
+
+////////////////////////////////////////////////////////
+    File file;
+    FileReader filereader;
+    FileWriter filewriter;
+    BufferedReader bufferedreader;
+    BufferedWriter bufferedwriter;
+
+////////////////////////////////////////////////////////
+    public Main(ConexionSQL conex) {
+        this.setContentPane(fondo);
+        initComponents();
+        this.conex = conex;
+    }
+
+    /*
+    private void deshabilitar() {
+        jButton2.setEnabled(false);
+        jButton4.setEnabled(false);
+        jButton5.setEnabled(false);
+        jButton6.setEnabled(false);
+    }
+     */
+ /*
+    private void habilitar() {
+        jButton2.setEnabled(true);
+        jButton4.setEnabled(true);
+        jButton5.setEnabled(true);
+        jButton6.setEnabled(true);
+    }
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton2.setText("Lista");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("SALIR");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Unidades"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jTable1.setCellSelectionEnabled(true);
+        jScrollPane1.setViewportView(jTable1);
+        jTable1.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+
+        jButton4.setText("Insertar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jButton5.setText("Borrar");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+
+        jButton6.setText("Actualizar");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ticket.png"))); // NOI18N
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3)
+                .addGap(40, 40, 40))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addComponent(jButton4)
+                        .addGap(78, 78, 78)
+                        .addComponent(jButton2)
+                        .addGap(56, 56, 56)
+                        .addComponent(jButton5)
+                        .addGap(78, 78, 78)
+                        .addComponent(jButton6))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(145, 145, 145)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(136, 136, 136)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(157, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton4)
+                    .addComponent(jButton2)
+                    .addComponent(jButton5)
+                    .addComponent(jButton6))
+                .addGap(54, 54, 54)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(47, 47, 47)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(58, 58, 58)
+                .addComponent(jButton3)
+                .addContainerGap(85, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        ControladorArticulos cont = new ControladorArticulos(conex);
+        try {
+            ArrayList<Articulo> list = cont.obtenerArticulos();
+            DefaultTableModel modelo = (DefaultTableModel) jTable1.getModel();
+            if (modelo.getRowCount() != 0) {
+                modelo.setRowCount(0);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                modelo.addRow(new Object[]{list.get(i).getNombre(), list.get(i).getUnidades()});
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        try {
+            // TODO add your handling code here:
+            conex.desconectar();
+            this.dispose();
+            System.exit(0);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        ControladorArticulos cont = new ControladorArticulos(conex);
+        String res = JOptionPane.showInputDialog(this, "Escribe el nombre del nuevo producto",
+                "Introduce un producto", JOptionPane.INFORMATION_MESSAGE);
+
+        String uds = JOptionPane.showInputDialog(this, "Indica las unidades",
+                "Introduce un producto", JOptionPane.INFORMATION_MESSAGE);
+
+        String consulta = "INSERT INTO compra VALUES('" + res + "','" + uds + "')";
+
+        try {
+
+            //conex.ejecutarInsDelUpd("INSERT INTO " + conex.getBD() + "(nombre, " + val + ") VALUES('" + res + "',0)");
+            conex.ejecutarInsDelUpd(consulta);
+            ArrayList<Articulo> list = cont.obtenerArticulos();
+            DefaultTableModel modelo = (DefaultTableModel) jTable1.getModel();
+            if (modelo.getRowCount() != 0) {
+                modelo.setRowCount(0);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                modelo.addRow(new Object[]{list.get(i).getNombre(), list.get(i).getUnidades()});
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        ControladorArticulos cont = new ControladorArticulos(conex);
+        String res = JOptionPane.showInputDialog(this, "Escribe el nombre del producto que quieras borrar",
+                "Borra un producto", JOptionPane.INFORMATION_MESSAGE);
+        try {
+            conex.ejecutarInsDelUpd("DELETE FROM compra WHERE nombre='" + res + "'");
+            ArrayList<Articulo> list = cont.obtenerArticulos();
+            DefaultTableModel modelo = (DefaultTableModel) jTable1.getModel();
+            if (modelo.getRowCount() != 0) {
+                modelo.setRowCount(0);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                modelo.addRow(new Object[]{list.get(i).getNombre(), list.get(i).getUnidades()});
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        // TODO add your handling code here:
+        ControladorArticulos cont = new ControladorArticulos(conex);
+        String res = JOptionPane.showInputDialog(this, "Escribe el nombre del producto que quieras modificar",
+                "Elige", JOptionPane.INFORMATION_MESSAGE);
+        String uni = JOptionPane.showInputDialog(this, "Escribe cuanto quieres del producto",
+                "Modificar", JOptionPane.INFORMATION_MESSAGE);
+        try {
+            conex.ejecutarInsDelUpd("UPDATE compra SET unidades=" + uni + " WHERE nombre = '" + res + "'");
+            ArrayList<Articulo> list = cont.obtenerArticulos();
+            DefaultTableModel modelo = (DefaultTableModel) jTable1.getModel();
+            if (modelo.getRowCount() != 0) {
+                modelo.setRowCount(0);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                modelo.addRow(new Object[]{list.get(i).getNombre(), list.get(i).getUnidades()});
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+
+        file = new File("yoshilista.txt");
+        try {
+            filewriter = new FileWriter(file, false);
+            bufferedwriter = new BufferedWriter(filewriter);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ControladorArticulos cont = new ControladorArticulos(conex);
+        try {
+            ArrayList<Articulo> list = cont.obtenerArticulos();
+
+            for (int i = 0; i < list.size(); i++) {
+
+                bufferedwriter.write("----- YOSHILIST ----\n" + "Producto: " + list.get(i).getNombre() + "\n" + "Unidades: " + list.get(i).getUnidades() + "\n");
+
+                JOptionPane.showMessageDialog(rootPane, "TICKET IMPRIMIDO CORRECTAMENTE!");
+
+            }
+            bufferedwriter.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }//GEN-LAST:event_jLabel1MouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration//GEN-END:variables
+class FondoPanel1 extends JPanel {
+
+        private Image imagen;
+
+        @Override
+        public void paint(Graphics g) {
+            imagen = new ImageIcon(getClass().getResource("/imagenes/fondo.jpg")).getImage();
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+            setOpaque(false);
+            super.paint(g);
+        }
+    }
+}
